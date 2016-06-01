2887d71809ed18cd884e0d02098063f72119d1cc
Andrey <andreykvital@gmail.com>
2016-05-31 22:25:27 -0300

    Change `Index` interface to be tight as possible for now
    Signed-off-by: Andrey <andreykvital@gmail.com>

--- index/index.go
+++ index/index.go
@@ -1,12 +1,7 @@
 package index
 
-import (
-	"github.com/nproc/kb/link"
-	"github.com/satori/go.uuid"
-)
+import "github.com/nproc/kb/link"
 
 type Index interface {
 	Add(link.Link) error
-	Remove(link.Link) error
-	Get(uuid.UUID) (link.Link, error)
 }
